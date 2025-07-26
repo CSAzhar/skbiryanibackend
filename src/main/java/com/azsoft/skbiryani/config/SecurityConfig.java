2148502498b72a8d8b1eb8a70234ca1fd057e892
CSAzhar <azhardarampur@gmail.com>
2025-07-27 01:44:58 +0530

    added

--- src/main/java/com/azsoft/skbiryani/config/SecurityConfig.java
+++ src/main/java/com/azsoft/skbiryani/config/SecurityConfig.java
@@ -76,8 +76,8 @@ public class SecurityConfig {
 		CorsConfiguration config = new CorsConfiguration();
 		config.setAllowedOrigins(List.of("http://localhost:5173" ,
 											"http://localhost:3000", 
-											"https://skbadmin2.s3.us-east-1.amazonaws.com",
-											"https://skbuser.s3.us-east-1.amazonaws.com"
+											"https://skbuser.s3.us-east-1.amazonaws.com",
+											"https://skbadmin.s3.us-east-1.amazonaws.com"
 											));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
