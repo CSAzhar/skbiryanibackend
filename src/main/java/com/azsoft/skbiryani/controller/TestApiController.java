1b056bb93d2508e38c14bbb6097049b498f21491
CSAzhar <azhardarampur@gmail.com>
2025-08-04 21:40:12 +0530

    added test file

---
+++ src/main/java/com/azsoft/skbiryani/controller/TestApiController.java
@@ -0,0 +1,19 @@
+package com.azsoft.skbiryani.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/ping")
+public class TestApiController {
+	
+	@GetMapping
+	public ResponseEntity<String> testApiForAws(){
+		return ResponseEntity.status(200).body("pong");
+	}
+
+}
